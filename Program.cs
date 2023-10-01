98758ce7732014c0cd98ce871326da3715347df8
Fittgross <william-gross@hotmail.com>
2023-10-01 16:06:54 +0200

    Big cleanup, added array of all card graphics

--- Program.cs
+++ Program.cs
@@ -7,34 +7,16 @@
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.WindowHeight = 50;
             Console.WindowWidth = 200;
+            Console.CursorVisible = false;
 
-            //PlayingTable board = new();
-            Card aceOfSpades = new("╭─────╮", "│A .  │", "│ /.\\ │", "│(_._)│", "│  │ V│", "╰─────╯");
-            //board.PrintBoard();
-            ////board.PrintCards(aceOfSpades);
-            //Deck.PrintAllCards();
-            //Console.ReadKey();
-            //Deck.ShuffleDeck();
-            //Console.ReadKey();
-            //Deck.PrintAllCards();
-            //Console.ReadKey();
-
-            Console.BackgroundColor = ConsoleColor.DarkGreen;
-            Console.ForegroundColor = ConsoleColor.Black;
             Graphics table = new Graphics();
             table.PrintBoard();
-            Graphics.PrintCards(aceOfSpades);
+            Graphics.PrintCard(Deck.AllCards[51]);
 
             Console.ReadKey();
-
-            //PlayingTable table = new PlayingTable();
-            List<int> dealerHand = new List<int>();
-            List<int> playerOneHand = new List<int>();
             
             BlackJack blackjack = new(new Graphics());
             Player playerOne = new Player("Kimmo");
-
-            
 
             blackjack.RunGame(playerOne);
         }
