8a75fa4f0ae9427538aca2f306b7cb5f4a72271c
Fittgross <william-gross@hotmail.com>
2023-10-09 21:21:26 +0200

    Added Utilities class and GetPlayers method

--- GameLogic.cs
+++ GameLogic.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Blackjack
 {
@@ -39,6 +40,5 @@ namespace Blackjack
             }
             return false;
         }
-
     }
 }
