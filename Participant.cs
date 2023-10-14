9ee6b2f15463bd5585f10801d9ef8e542c415b8e
Fittgross <william-gross@hotmail.com>
2023-10-14 22:05:34 +0200

    #67 Fixed 16 Hand-errors in Utilities

--- Participant.cs
+++ Participant.cs
@@ -5,11 +5,9 @@
         public Participant()
         {
             Hands = new List<Hand>();
-            SplitHand = null;
         }
 
         public List<Hand> Hands { get; set; }
-        public List<Card>? SplitHand { get; set; }
         public PlayerAction LatestAction { get; set; }
     }
 }
