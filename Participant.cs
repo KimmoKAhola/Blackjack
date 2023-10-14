b732b9d7ba20d5476d655a74f4d2e46b22ce202c
Fittgross <william-gross@hotmail.com>
2023-10-14 18:21:21 +0200

    Added List<Card> SplitHand property to Participant

--- Participant.cs
+++ Participant.cs
@@ -5,9 +5,11 @@
         public Participant()
         {
             Hand = new List<Card>();
+            SplitHand = null;
         }
 
         public List<Card> Hand { get; set; }
+        public List<Card> SplitHand { get; set; }
         public PlayerAction LatestAction { get; set; }
         public int HandSum()
         {
